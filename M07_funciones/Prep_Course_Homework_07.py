cdf7c2e55639dcaab6dbddc8cf55989987afc1a1
Tonatiuh Velzquez Rojas <tonatiuh_vr95@hotmail.com>
2023-10-12 17:17:44 -0600

    ejercicios 7,3 incompleto

--- M07_funciones/Prep_Course_Homework_07.py
+++ M07_funciones/Prep_Course_Homework_07.py
@@ -7,24 +7,44 @@
 
 # In[1]:
 
-
+def ver_prim(in_1):
+    es_primo = True
+    for i in range(2, in_1):
+        if in_1 % i == 0:
+            es_primo = False
+            break
+    return es_primo
 
 
 
 # 2) Utilizando la función del punto 1, realizar otra función que reciba de parámetro una lista de números y devuelva sólo aquellos que son primos en otra lista
 
 # In[25]:
+def primo_array(in_2):
+    a=[]
+    for x in in_2:
+        if (ver_prim(int(x))==True):# en sentencias de v/f se puede obviar el verdadero
+            a.append(x)
+    return a
 
-
-
+z=[1,2,3,4,5,6,7,8,9,10,11]
+print(primo_array(z))
 
 # 3) Crear una función que al recibir una lista de números, devuelva el que más se repite y cuántas veces lo hace. Si hay más de un "más repetido", que devuelva cualquiera
 
 # In[33]:
 
-
-
-
+def moda(in_a):
+    rep=[]
+    visit=[False]
+    if len(in_a)==0:
+        return None
+    for x in in_a:
+        for y in in_a:
+            print("x es:",x, "y es: ",y)
+print("ejercicio 3 ")
+z=[1,2,3,4,5,6,7,8,9,10,11]
+print(moda(z))
 
 # 4) Crear una función que convierta entre grados Celsius, Farenheit y Kelvin<br>
 # Fórmula 1	: (°C × 9/5) + 32 = °F<br>
