3ecaf1858cc8d060e60496f390a3a54a2897e7ae
Tonatiuh Velzquez Rojas <tonatiuh_vr95@hotmail.com>
2023-10-17 10:55:28 -0600

    prueba 2

--- M08_clasesyOOP/ejemplo_1main.py
+++ M08_clasesyOOP/ejemplo_1main.py
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
 from class_1 import *
 
 gato_1=Gato("pompon",2,"siames","femenino") 
 gato_1.presentar()
-=======
-from class_1 import *
-
-gato_1=Gato("pompon",2,"siames","femenino") 
-gato_1.presentar()
->>>>>>> 6c4a481ef354f957a74357b8dd9b44a3777712bb
-#ss
+#ssart
+#fff
