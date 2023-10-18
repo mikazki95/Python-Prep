fe289a3296abcb737c0be9feeefa48254166b564
Tonatiuh Velzquez Rojas <tonatiuh_vr95@hotmail.com>
2023-10-18 16:53:19 -0600

    checar errores clase

--- M09_errorhandling/Prep_Course_Homework_09.py
+++ M09_errorhandling/Prep_Course_Homework_09.py
@@ -9,13 +9,73 @@
 
 
 
+def moda(in_a):
+    count_max=0
+    visit=[]
+    maxim=0
+    result=[0,0]
+    if type(in_a)==list:
+        if len(in_a)==0:
+            return None
+        else:
+            for x in in_a:
+                if (type(x)==int) or (type(x)==float):
+                    visit.append(False)
+                else:
+                    print("la lista solo debe contener numeros")
+            index_a=0
+            while index_a<len(in_a):
+                x=in_a[index_a]
+                a=in_a.count(x)
+                index_a+=1
+                if a > count_max:
+                    count_max=a
+                    maxim=x
+                    result[0]=maxim
+                    result[1]=count_max
+            return result
+    else :
+        print("se espera una lista de numeros")
+print("ejercicio 3 ")
+z=[2,21,13,44,5,6,7,8,9,10,11,21,21]
+print(moda(z))
 
 # 2) En la función que hace la conversión de grados, validar que los parámetros enviados sean los esperados, de no serlo, informar cuáles son los valores esperados.
 
 # In[5]:
 
 
-
+def con_temp (dato,data_in,data_out):
+    error="error de tipo de dato, espesifique con: 'k' para kelvin, 'c' para celcius o 'f' para farenheit"
+    if ((type(dato)==int)or(type(dato)==float)and(type(data_in)==str)and(type(data_out)==str)):
+        match data_in:
+            case 'k':
+                if data_out=='f':
+                    return (1.8*(dato-273)+32)
+                elif data_out=='c':
+                    return (dato-273.15)
+                else:
+                    return error
+            case 'c':
+                if data_out=='f':
+                    return ((dato*9/5)+32)
+                elif data_out=='k':
+                    return (dato+273.15)
+                else:
+                    return error
+            case 'f':
+                if data_out=='c':
+                    return ((dato-32)*5/9)
+                elif data_out=='k':
+                    return (273+(dato-32)/1.8)
+                else:
+                    return error
+            case _:
+                return error
+    else:
+        return error
+print("ejercicio 4 ")
+print(con_temp(147,'f','k'))
 
 
 # 3) Importar el modulo "unittest" y crear los siguientes casos de pruebas sobre la clase utilizada en el punto 2<br>
@@ -26,6 +86,7 @@
 # Se puede usar "raise ValueError()" en la creación de la clase para verificar el error. Investigar sobre esta funcionalidad.
 
 # In[9]:
+import unittest 
 
 
 
